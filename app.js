7d88881791e9ec8401bd1f17a3dc7aba87e937fe
ShrutiGupta <ShrutiGupta1604@gmail.com>
2023-10-27 17:48:36 +0530

    delete ToDo list

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var todo = []
+var counter = 0
 
 
 function addTodo(event)
@@ -8,8 +9,13 @@ function addTodo(event)
     let tempTodoText = document.getElementById("todoText").value
     console.log("Text added: "+tempTodoText)
 
-    console.log(todo)
-    todo.push(tempTodoText)
+    console.log(todo) 
+    let tempTodo = {
+        id: counter++,
+        text: tempTodoText,
+    }
+    
+    todo.push(tempTodo)
     console.log(todo)
 
     
@@ -19,10 +25,35 @@ function addTodo(event)
     todoList.innerHTML = ""
     todo.forEach(element=>
         {
-            todoList.innerHTML += "<li>"+element+"</li>"
+            todoList.innerHTML += "<li>"+element.text+" <button onclick=\"deleteTodo("+element.id+")\">Delete</button></li>"
         })
 
 
     }
+    function deleteTodo(id)
+    {
+        console.log("----Delete todo-----")
+        console.log("deleted id " +id)
+        todo = todo.filter(element=>{
+            return element.id != id 
+        })
+
+        let todoList = document.getElementById("todoList")
+    todoList.innerHTML = ""
+
+    todo.forEach(element=>
+        {
+           return  todoList.innerHTML += "<li>"+element.text+" <button onclick=\"deleteTodo("+element.id+")\">Delete</button></li>"
+        })
+        console.log(todo)
+
+        
+    }
+
+    function checkboxListener(id)
+    {
+        console.log("id: "+id)
+    }
+    
 
    
